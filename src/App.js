97bd38ebddae55eba3000143dcbb18e1009b69e6
Rauno Lillemets <32326803+RaunoL@users.noreply.github.com>
2020-05-26 23:07:19 +0300

    cleaned up default files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router
+} from "react-router-dom";
+import "./styles/main.scss";
+import Login from "./views/Login";
+import Main from "./views/Main";
+import Edit from "./views/Edit";
+import Create from "./views/Create";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/main" component={Main}></Route>
+        <Route exact path="/edit" component={Edit}></Route>
+        <Route exact path="/create" component={Create}></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/views/Edit.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Edit(){
+    return(
+        <div><i class="fas fa-pen"></i></div>
+    )
+}
+export default Edit
